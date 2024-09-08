8cb07d636e6cb39100edb1604e9295c412c37027
송현준 <70439776+SHJ0515@users.noreply.github.com>
2024-09-09 01:21:54 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println(s.charAt(n - 1));
+    }
+}
