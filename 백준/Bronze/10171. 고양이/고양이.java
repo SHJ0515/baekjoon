910958d6ba29d8f183d0787f5751893e2c47f938
Song HyunJun <70439776+SHJ0515@users.noreply.github.com>
2025-11-19 16:29:35 +0900

    [Bronze V] Title: 고양이, Time: 164 ms, Memory: 17368 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\");
+        sb.append("\n");
+        sb.append(" )  ( ')");
+        sb.append("\n");
+        sb.append("(  /  )");
+        sb.append("\n");
+        sb.append(" \\(__)|");
+
+        System.out.println(sb.toString());
+    }
+}
