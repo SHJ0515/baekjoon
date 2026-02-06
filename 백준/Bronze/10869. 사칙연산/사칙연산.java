4ee28c960467ab4d67db0f2182520afc3bbbab28
Song HyunJun <70439776+SHJ0515@users.noreply.github.com>
2026-02-06 16:22:11 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String[] split = br.readLine().split(" ");
+
+        int n = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+
+        method(n, m);
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void method(int a, int b) {
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
