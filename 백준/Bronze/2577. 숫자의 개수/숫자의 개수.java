d705811cb36bc6599a80e8e683d94439fe4ef206
Song HyunJun <70439776+SHJ0515@users.noreply.github.com>
2026-02-10 20:06:30 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,33 +1,34 @@
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        int[] arrCount = new int[10]; // 0~9 나온 횟수 count
-        int num = 1;
+        //StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < 3; i++) {
-            int n = Integer.parseInt(br.readLine());
-            num = num * n;
-        }
+        int[] count = new int[10]; // 0~9 횟수
 
-        while (num != 0){
-            int tmp = num % 10;
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        int num = a * b * c;
+
+        while (num > 0){
+            int last = num % 10;
+            count[last]++;
             num = num / 10;
-            arrCount[tmp]++;
         }
 
-        for (int i : arrCount) {
+        for (int i : count) {
             System.out.println(i);
         }
 
-        br.close(); // BufferedReader 닫기
-        //bw.flush();
-        //bw.close();
+        bw.flush();
+        bw.close();
     }
 }
