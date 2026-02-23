1a6a95edbb723ffaa8aefdf921ba28e2bd0d3209
Song HyunJun <70439776+SHJ0515@users.noreply.github.com>
2026-02-23 22:09:49 +0900

    [Silver IV] Title: 카드2, Time: 152 ms, Memory: 29160 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,32 +1,31 @@
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        Queue<Integer> queue = new ArrayDeque<>();
 
+        Deque<Integer> queue = new ArrayDeque<>();
 
         int n = Integer.parseInt(br.readLine());
-
         for (int i = 1; i <= n; i++) {
-            queue.add(i);
-        }
-        
-        while (queue.size() != 1){
-            queue.poll();
-            Integer poll = queue.poll();
-            queue.add(poll);
+            queue.addLast(i);
         }
 
-        Integer peek1 = queue.peek();
-        System.out.println(peek1);
 
-        br.close();
+        while (queue.size() != 1) {
+            queue.pollFirst();
+            if (queue.size() != 1) {
+                Integer i = queue.pollFirst();
+                queue.addLast(i);
+            }
+        }
+
+        System.out.println(queue.peek());
+
+        bw.flush();
         bw.close();
     }
 }
