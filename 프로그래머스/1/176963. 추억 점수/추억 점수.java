4bc9320fde1a343dd5404063f8ee526350b7b41d
Song HyunJun <70439776+SHJ0515@users.noreply.github.com>
2026-07-04 20:49:17 +0900

    [level 1] Title: 추억 점수, Time: 5.27 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        for (int i = 0; i < photo.length; i++) {
+            int tmp = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                for (int k = 0; k < name.length; k++) {
+                    if (photo[i][j].equals(name[k])){
+                        tmp = tmp + yearning[k];
+                        break;
+                    }
+                }
+            }
+            answer[i] = tmp;
+        }
+        return answer;
+    }
+}
